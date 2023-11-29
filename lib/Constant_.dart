f0df71357a6dabf0d29cf001b6c8b78006201a00
PC 01 <muthaiyanraja1999@gmail.com>
2023-11-29 20:10:22 +0530

    @HyBrid

--- lib/Constant_.dart
+++ lib/Constant_.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class Constants {
-
   Color primaryAppColor = const Color(0xff5D5FEF);
    String avator = 'assets/images/ellipse-92-bg.png';
    String docid="HbRb0XiGytHNAWtiaUEN";
